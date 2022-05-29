52c18e5172922050ffc4e9ee307a96a9f4f2d48c
melodyn <samelodyn@gmail.com>
2022-05-29 20:48:20 +0300

    refactoring cors

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
 const allowCors = (app) => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
+  console.log('request')
 
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
+    console.log('OPTIONS', res.headers);
+    res.status(200).json({ body: 'OK' });
     return;
   }
 
